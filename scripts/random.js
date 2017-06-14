4e2a68d884b6bf57526e9fce6b5f88274c3f0bb6
codeanticode <andres.colubri@gmail.com>
2017-06-13 20:00:19 -0400

    added title page and random walk script

---
+++ scripts/random.js
@@ -0,0 +1,78 @@
+var viz_function = function(p) {
+  
+  var words = new Array("drawing",
+                        "statistics", 
+                        "simulation", 
+                        "visualization",                         
+                        "processing",
+                        "interaction",
+                        "shaders",
+                        "performance",
+                        "live",
+                        "data",
+                        "information",
+                        "prediction",
+                        "disease");
+  
+  var px, py;
+  var x, y;
+  var numWords = words.length;
+  var maxIter = 1500;
+  var lastCount = 0;
+  var prob = 2 *  numWords / maxIter;  
+  
+  p.setup = function() {  
+    var canvas = p.createCanvas(800, 600);
+    canvas.parent('random_walk');
+    p.frameRate(30);
+    p.noLoop();
+    p.fill(0,255,0);
+    p.background(255);
+    p.textAlign(p.CENTER);
+    x = px = 0.5 * p.width;
+    y = py = 0.5 * p.height;
+    p.textSize(18);
+  }
+
+  p.draw = function() {    
+    if (px < 50) x = px + p.random(0, 10);
+    else if (p.width - 50 < px) x = px + p.random(-10, 0);    
+    else x = px + p.random(-10, 10);
+    
+    if (py < 50) y = py + p.random(0, 10);
+    else if (p.height - 50 < py) y = py + p.random(-10, 0);    
+    else y = py + p.random(-10, 10);
+    
+    if (p.random(1) < prob && 0 < words.length && lastCount + 30 < p.frameCount) {
+      var w = words.splice(0, 1);
+
+      p.noStroke();
+      p.fill(100);
+      p.text(w, x, y);
+      lastCount = p.frameCount;
+    }
+
+    p.stroke(200);
+    p.line(px, py, x, y);
+    px = x;
+    py = y;  
+    
+    if (maxIter < p.frameCount) {
+       p.noLoop();
+    }
+    
+	p.mouseMoved = function() {
+	  if (!p.looping && p.frameCount < maxIter) {
+	    p.loop()
+	  }
+	}
+	
+	p.keyPressed = function() {
+	  if (!p.looping && p.frameCount < maxIter) {
+	    p.loop()
+	  }	
+	}	
+  }
+}
+
+var viz = new p5(viz_function)
--- source.md
+++ source.md
@@ -1,3 +1,13 @@
-### Andres' portfolio
+<img height="200" src="http://portfolio.andrescolubri.net/images/randomwalk.png" style="background:none; border:none; box-shadow:none"/>
 
-### Coming soon!
+drawing with code:
+
+a (sometimes random)  walk
+
+through art, code, and science
+
+H:
+
+<div id="random_walk" style="background:none; border:none; box-shadow:none"></div>
+
+H:
